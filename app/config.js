aae6f062d7bd023240444705b4dd309cec5cb65f
demandre <joris.demandre@gmail.com>
2019-04-11 12:26:02 +0200

    update mongodb database url

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    mongodb: 'mongodb://localhost:27017/socialobjects'
+    mongodb: 'mongodb://localhost:27017/articleApi'
   },
   production: {
-    mongodb: 'mongodb://localhost:27017/socialobjects'
+    mongodb: 'mongodb://localhost:27017/articleApi'
   }
 }
