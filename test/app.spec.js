59838aa19a3d6b2d8047ca75cfb179b11a3a29d4
demandre <joris.demandre@gmail.com>
2019-04-11 15:09:22 +0200

    mutualized tests to clear duplicate server error

--- test/app.spec.js
+++ test/app.spec.js
@@ -11,6 +11,41 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 /**
+ * GET /user
+ */
+describe('GET /user', () => {
+  it('POST /create should create an user', (done) => {
+    //const result = '{"name":"cyril","age":"30","sexe":"male"}';
+    const payload = {"name":"cyril","age":'30',"sexe":"male"};
+
+    chai.request(app)
+      .post('/user/create')
+      .send(payload)
+      .end((err, res) => {
+          res.should.have.status(200);
+          //res.text.should.be.eql(result); Can't verify as object_id is random
+
+          done();
+      });
+  });
+
+  it('POST /create should check the payload body is false', (done) => {
+    const result = '{"errors":[{"parameter":"nme","value":"tutu","message":"Unexpected value."},{"parameter":"ag","value":40,"message":"Unexpected value."},{"parameter":"gende","value":"male","message":"Unexpected value."},{"parameter":"name","message":"Required value."}]}';
+    const payload = {'nme': 'tutu','ag': 40,'gende': 'male'};
+
+    chai.request(app)
+      .post('/user/create')
+      .send(payload)
+      .end((err, res) => {
+          res.should.have.status(400);
+          res.text.should.be.eql(result);
+
+          done();
+      });
+  });
+});
+
+/**
  * GET /article
  */
 describe('GET /article', () => {
