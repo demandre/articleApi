3b6989340f791701d80bb3e5920e509a1d368fa5
demandre <joris.demandre@gmail.com>
2019-04-11 14:08:34 +0200

    add article create tests

--- test/article.spec.js
+++ test/article.spec.js
@@ -11,15 +11,14 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 /**
- * GET /user
+ * GET /article
  */
-describe('GET /user', () => {
-  it('POST /create should create an user', (done) => {
-    //const result = '{"name":"cyril","age":"30","sexe":"male"}';
-    const payload = {"name":"cyril","age":'30',"sexe":"male"};
+describe('GET /article', () => {
+  it('POST /create should create an article', (done) => {
+    const payload = {"title":"Great article","content":'This is an article',"cover_img_url":"test url"};
 
     chai.request(app)
-      .post('/user/create')
+      .post('/article/create')
       .send(payload)
       .end((err, res) => {
           console.log(res);
@@ -31,11 +30,11 @@ describe('GET /user', () => {
   });
 
   it('POST /create should check the payload body is false', (done) => {
-    const result = '{"errors":[{"parameter":"nme","value":"tutu","message":"Unexpected value."},{"parameter":"ag","value":40,"message":"Unexpected value."},{"parameter":"gende","value":"male","message":"Unexpected value."},{"parameter":"name","message":"Required value."}]}';
+    const result = '{"errors":[{"parameter":"nme","value":"tutu","message":"Unexpected value."},{"parameter":"ag","value":40,"message":"Unexpected value."},{"parameter":"gende","value":"male","message":"Unexpected value."},{"parameter":"title","message":"Required value."},{"parameter":"content","message":"Required value."}]}';
     const payload = {'nme': 'tutu','ag': 40,'gende': 'male'};
 
     chai.request(app)
-      .post('/user/create')
+      .post('/article/create')
       .send(payload)
       .end((err, res) => {
           res.should.have.status(400);
