14177626d71a46559e4ce977eb74df5ae130b18c
demandre <joris.demandre@gmail.com>
2019-04-11 15:35:12 +0200

    add comment and date to article model and create

--- app/models/article.js
+++ app/models/article.js
@@ -3,7 +3,9 @@ const Schema = require('mongoose').Schema;
 module.exports = new Schema({
   'title': String,
   'content': String,
-  'cover_img_url': String
+  'cover_img_url': String,
+  'date': String,
+  'comment': Array
 }, {
   'collection': 'articles',
   'versionKey': false
